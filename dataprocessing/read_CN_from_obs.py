ca050579982de4e0f30b1f799aeeabc6f8fcd7d7
jason <leshanlijiayu@126.com>
2025-07-28 13:22:01 +0800

    add readCN 1day func

--- dataprocessing/read_CN_from_obs.py
+++ dataprocessing/read_CN_from_obs.py
@@ -3,6 +3,11 @@ import os
 import pandas as pd
 import utils
 
+def list_files_in_directory(directory_path):
+    if not os.path.exists(directory_path):
+        raise ValueError(f"Directory {directory_path} does not exist.")
+    return [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
+
 def read_CN_value_from_obs(station_name, SS_variables, satellite_code, input_folder, output_folder, start_date_str, end_date_str):
     def list_files_in_directory(directory_path):
         if not os.path.exists(directory_path):
@@ -74,11 +79,6 @@ def read_CN_value_from_obs(station_name, SS_variables, satellite_code, input_fol
 # Read CN value from obs all at once
 def read_CN_value_from_obs_AAO(station_name, SS_variables, input_folder, output_folder, start_date_str,
                                end_date_str):
-    def list_files_in_directory(directory_path):
-        if not os.path.exists(directory_path):
-            raise ValueError(f"Directory {directory_path} does not exist.")
-        return [f for f in os.listdir(directory_path) if os.path.isfile(os.path.join(directory_path, f))]
-
     # 解析 start_date 和 end_date
     start_date = pd.to_datetime(start_date_str, format='%Y%j')
     end_date = pd.to_datetime(end_date_str, format='%Y%j')
@@ -143,6 +143,48 @@ def read_CN_value_from_obs_AAO(station_name, SS_variables, input_folder, output_
             else:
                 print(f"Warning: {SS_variable} not found in file {file}")
 
+def read_CN_value_from_obs_1D(SS_variables ,input_folder, output_folder, date_str):
+    # 解析 date
+    date = pd.to_datetime(date_str, format='%Y%j')
+    # 获取输入文件夹中的所有文件
+    files_array = sorted(list_files_in_directory(input_folder))
+    # 创建输出目录
+    utils.create_directory_if_not_exists(output_folder)
+    # 处理每个文件并保存对应的CSV文件
+    for file in files_array:
+        file_path = os.path.join(input_folder, file)
+        obs_info = gp.read_obsFile(file_path)
+
+        # 对每个 SS_variable 提取数据并按 code 保存
+        for SS_variable in SS_variables:
+            if SS_variable in obs_info.observation:
+                # 创建一个字典来保存不同 code 的数据
+                satellite_data = {}
+
+                for entry in obs_info.observation[SS_variable].items():
+                    timestamp, code = entry[0]
+                    value = entry[1]
+
+                    # 如果 code 以 "G" 开头，保存到对应的列表中
+                    if code.startswith('G'):
+                        if code not in satellite_data:
+                            satellite_data[code] = []
+                        satellite_data[code].append((timestamp, value))
+
+                # 将不同 code 的数据分别保存到不同的 CSV 文件中
+                for code, data in satellite_data.items():
+                    if data:
+                        output_filename = f'{filename_base[0:4]}_{filename_base[12:19]}_CN_{code}_{SS_variables[0]}.csv'
+                        output_path = os.path.join(output_folder, output_filename)
+
+                        if not os.path.exists(output_path):  # 检查输出文件是否已存在
+                            df = pd.DataFrame(data, columns=['Timestamp', SS_variable])
+                            df.to_csv(output_path, index=False)
+                            print(f"Saved: {output_path}")
+                        else:
+                            print(f"File {output_path} already exists. Skipping...")
+            else:
+                print(f"Warning: {SS_variable} not found in file {file}")
 
 def read_CN_value_from_obs_allfile(SS_variables, satellite_code, input_folder, output_folder
                                ):
